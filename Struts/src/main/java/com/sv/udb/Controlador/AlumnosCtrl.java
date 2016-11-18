f0b9424074f9f988aaa65cd9984565a98dd4d3e8
fic <fic@fic-Compaq-CQ45-Notebook-PC>
2016-11-17 20:27:21 -0600

    Probando commit ya con paquetes

---
+++ Struts/src/main/java/com/sv/udb/Controlador/AlumnosCtrl.java
@@ -0,0 +1,15 @@
+package com.sv.udb.Controlador;
+
+import com.opensymphony.xwork2.*;
+
+public class AlumnosCtrl extends ActionSupport{
+	
+	
+	public String execute() throws Exception {
+		
+		return SUCCESS;
+	}
+
+	
+
+}
